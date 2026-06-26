f6c377dbcdf51a51d4ff17a95583f10286be393c
Chris Henson <chrishenson.net@gmail.com>
2026-06-26 13:54:09 -0400

    more cleanup

--- LF/Typeclasses.lean
+++ LF/Typeclasses.lean
@@ -13,6 +13,8 @@ import SFLMeta.SlideBreak
 import SFLMeta.Solution
 import SFLMeta.Terse
 
+set_option autoImplicit false
+
 open Verso.Genre Manual
 open SFLMeta
 
@@ -25,39 +27,37 @@ file := "Typeclasses"
 %%%
 
 
-So far we have discussed what is sometimes called _parametric polymorphism_, where in Lean we can
-declare a type variable
+So far we have discussed *parametric polymorphism*, where in Lean we can declare a type variable
 
 ```lean
 variable (α : Type)
 ```
 
-without any further information about the details of the type `α`. In this we we are able to work
-with type like {InlineLean.lean}`List α`, and can write function like {name}`List.reverse` or
-{name}`List.length` that only operate on the structure of a list independently of its contents,
-along with corresponding proofs like {name}`List.length_reverse` that are similarly structural in
-nature.
+without any further information about the details of the type `α`. Here we are able to work
+with a type like {InlineLean.lean}`List α`, and can write functions like {name}`List.reverse` or
+{name}`List.length` that only operate on the structure of a list independently of its contents.
+Along with these he have corresponding proofs like {name}`List.length_reverse` that are similarly
+structural in nature.
 
-This is somewhat limiting though, as this means that there is no way that we can inspect or
-non-trivially use any particular `a : α`. What we might want in some situations rather than `α`
-being completely generic is to partially specify its behavior. In Lean, this happens through a form
-of "ad hoc polymorphism" called *typeclasses*. This concept originating in the Haskell programming
-language, and is analogous to concepts such as traits in Rust or interfaces in Java that you may
-already be familiar with.
+This is somewhat limiting, as we cannot inspect or non-trivially use any particular `a : α`. What we
+might want in some situations rather than `α` being completely generic is to partially specify its
+behavior. In Lean, this happens through a form of ad hoc polymorphism" called *typeclasses*. This
+concept originated in the Haskell programming language, and is analogous to features you may be
+familiar with in other languages such as traits in Rust or interfaces in Java.
 
-# A First Example: Inhabited Types
+# First Example: Inhabited Types
 
-As an example, suppose that I wanted a way to specify that a type has at least one inhabitant, that
-is that it is not empty. In Lean, we could express this as the typeclass
+Suppose that I wanted to specify that a type has at least one inhabitant, in other words a non-empty
+type. In Lean, we could express this as the typeclass
 
 ```lean
 class HasOne (α : Type) where
   one : α
 ```
 
-which explicitly carries around an element `one : α` as a witness. After defining this typeclass,
-we can now provide *instances* that show it is satisfied for a particular type. As an example,
-we could declare an instance
+which explicitly carries an element `one : α` as a witness of non-emptiness. After defining this
+typeclass, we can now provide *instances* that show it is satisfied for a particular type. As an
+example, we could declare an instance
 
 ```lean
 instance instHasOneNat : HasOne Nat where
@@ -84,7 +84,7 @@ I say recall below, but I have no idea where/if we teach this.. - CGH
 :::
 
 and based on the type annotations Lean will infer which of these instances you intended to use.
-When in doubt, you can also use the option `pp.all` like:
+When in doubt, you can also use the option `pp.all`
 
 
 ```lean
@@ -104,10 +104,15 @@ also check for the existence of a typeclass instance using the `#synth` command
 
 # Proof Carrying Typeclasses
 
-In most languages that support typeclasses this is the extent of what a typeclass can enforce,
-carrying the data of a type, but relying on the author to enforce any invariants. As an example,
-suppose that we wanted to express the idea that a type has at least two elements. A first attempt
-might be
+:::dev
+I say data to mean "not in `Prop`", is that a Lean-ism that's been explained? --CGH
+:::
+
+Notably the above examples enforce no conditions on the data that they carry, and we could provide
+any value that we would like for {name}`HasOne.one`. In most languages that support typeclasses it
+is not possible to enforce the notion of "lawfulness" as part of the typeclass, and it falls to
+the author to ensure that any desired invariants are satisfied. As an example, suppose that we
+wanted to express the idea that a type has at least two elements. A first attempt might be
 
 ```lean
 class HasTwoAttempt (α : Type) where
@@ -143,11 +148,6 @@ instance : HasTwo Nat where
 Following the pattern of {name}`HasOne` and {name}`HasTwo`, define a class `HasThree` that
 specifies a type with (at least) three distinct elements.
 
-:::dev
-David's example repo had this `-- SOLUTION` syntax for when we want the student to write definitions
-in an exercise, but it seems to not be working with the Verso build -- CGH
-:::
-
 ```lean
 class HasThree (α : Type) where
   one : α
@@ -177,23 +177,8 @@ instance : HasThree Nat where
 
 # Notation Typeclasses
 
-:::dev
-In a couple of places I want to reference a previous chapter that is not yet in Verso format,
-so I leave a <LINK> placeholder. At the moment, this is written as if after the (Rocq) `IndProp`
-chapter.
-:::
-
-In previous chapters we have seen how polymorphism in Lean allows us to declare a type variable
-
-
-that work with functions like {name}`List.reverse`. This variety of polymorphism
-is called *parametric* because it works identically for any given type, so that
-whether we are working with {InlineLean.lean}`List.reverse (α := Nat)` or with
-{InlineLean.lean}`List.reverse (α := Int)`, proofs we have constructed like
-{name}`List.length_reverse` are independent of the type used.
-
-There are some functions we can write however where it is unclear how we would write a polymorphic
-equivalent. As an example, consider the follow function for lists of natural numbers:
+Another use for typeclasses that may be familiar from other languages is to overload a given piece
+of syntax. To start, consider the (non-polymorphic) definition
 
 ```lean
 def List.elem_nat (a : Nat) (xs : List Nat) : Bool :=
@@ -227,17 +212,19 @@ error: failed to synthesize instance of type class
 Hint: Type class instance resolution failures can be inspected with the `set_option trace.Meta.synthInstance true` command.
 -/
 #guard_msgs in
-def List.elem_poly {α : Type} (a : α) (xs : List α) : Bool :=
+def List.elem_poly' {α : Type} (a : α) (xs : List α) : Bool :=
   match xs with
   | [] => false
-  | b :: tl => bif a == b then true else elem_poly a tl
+  | b :: tl => bif a == b then true else elem_poly' a tl
 ```
 
-that mentions a {deftech}_typeclass_ {name}`BEq`. Perhaps a better question is what exactly
-the `==` notation meant in our original {name}`List.elem_nat`. Let's look at a usage of this
-notation with natural numbers, but turn off notation:
+that mentions a typeclass {name}`BEq`. Perhaps a better question is what exactly the `==` notation
+meant in our original {name}`List.elem_nat`. Let's look at a usage of this notation with natural
+numbers, but turn off notation:
 
 ```lean
+/-- info: BEq.beq 1 2 : Bool -/
+#guard_msgs in
 set_option pp.notation false in
 #check 1 == 2
 ```
@@ -262,16 +249,25 @@ instance (priority := low) : BEq Nat where
 ```
 
 where we are specifying that the usage of `==` for natural numbers should correspond to the
-expected `Nat.beq`. This process of
-
-What is happening behind the scenes when we use the `a == b` notation is that Lean sees that
-this
+expected `Nat.beq`. So in order to write a function that uses the notation for boolean equality over
+a generic type, we need a way to express that there is some instance for {InlineLean.lean}`BEq α`.
+This is done using *instance implicits*, where we place a desired typeclass in square brackets. Our
+corrected definition is then
 
 ```lean
-#synth BEq Nat
+def List.elem_poly {α : Type} [BEq α] (a : α) (xs : List α) : Bool :=
+  match xs with
+  | [] => false
+  | b :: tl => bif a == b then true else elem_poly a tl
 ```
 
-# Maps?
+where Lean can now infer the usage of `==` is the one that we have provided.
+
+# Maps
+
+:::dev
+Maps could go here as an example to reinforce this? --CGH
+:::
 
 # Reflection
 
@@ -325,7 +321,6 @@ I assume here that we still defined existential quantification and these definit
 skipped the reflection part. Redefining because I don't really understand the state the previous
 files are in to import things.
 
-
 -- CGH
 :::
 
@@ -353,7 +348,7 @@ theorem isEven_one : isEven 1 = false := rfl
 theorem isEven_succ_succ (n : Nat) : isEven (n + 2) = isEven n := rfl
 
 theorem double_zero : double 0 = 0 := by rfl
-theorem double_succ (n : Nat) : double (n + 1) = double n + 2 := by rfl
+theorem double_succ (n : Nat) : double (n + 1) = double n + 2 := rfl
 
 end
 
@@ -370,7 +365,6 @@ theorem isEven_succ (n : Nat) : isEven (n + 1) = ! isEven n := by
     rewrite [isEven_succ_succ, ih, Bool.not_not]
     rfl
 ```
-
 
 As an example, we can write
 
@@ -431,6 +425,10 @@ theorem  isEven_double_exists (n : Nat) :
       rfl
 ```
 
+In Lean, the way that we express a proposition is decidable is via the {name}`Decidable` typeclass.
+In particular, one way that we can use this typeclass is by proving that the proposition is
+equivalent to some boolean valued function:
+
 ```lean
 theorem isEven_iff_Even {n : Nat} : isEven n = true ↔ Even n where
   mp h := by
@@ -444,9 +442,13 @@ theorem isEven_iff_Even {n : Nat} : isEven n = true ↔ Even n where
     exact even_double k
 ```
 
+and then declare an instance that makes use of this:
+
 ```lean
 instance (n : Nat) : Decidable (Even n) := decidable_of_decidable_of_iff isEven_iff_Even
 ```
+
+Now we are able to complete such proofs by computation using the {tactic}`decide` tactic:
 
 ```lean
 section
@@ -461,4 +463,42 @@ example : ¬ Even 101 := by decide
 example : ∀ n < 10, Even (2 * n) := by decide
 example : ∀ n < 10, Even (2 * n) ∧ ¬ Even (2 * n + 1) := by decide
 end
+```
+
+:::dev
+This is essentially the "Case Study: Improving Reflection" section in the Rocq `IndProp`
+chapter. Not sure exactly how we want this ported -- CGH
+:::
+
+:::dev
+`Decidable` only carries the proposition and not the boolean, so one direction of
+`reflect_iff` is easily translated, but the other is a bit different. I list some theorems
+below but you should Loogle and see if that's what you want. Some the the proofs can be a bit
+advanced if you follow core, or otherwise a bit circular. -- CGH
+:::
+
+```lean
+#check decidable_of_bool
+
+example {P : Prop} (b : Bool) (h : b = true ↔ P) : Decidable P := by
+  by_cases hb : b
+  · apply isTrue
+    simp [← h, hb]
+  · apply isFalse
+    simp [← h, hb]
+```
+
+```lean
+#check decide_eq_false_iff_not
+#check decide_eq_true_iff
+```
+
+:::dev
+I'm not sure what part of the signature here is important to translate. Is the point the
+`Bool`/`Prop` mismatch? -- CGH
+:::
+
+```lean
+example (a : α) [BEq α] [LawfulBEq α] (xs : List α) (neq : xs.filter (a == ·) ≠ []) : a ∈ xs := by
+  sorry
 ```
