d5cedf942681397ce6ce9409b19e8cdc1ece8559
Roger Burtonpatel <burtonpatel1@gmail.com>
2026-07-13 15:36:03 +0200

    Note in indprop on `in` examples

--- LF/IndProp.lean
+++ LF/IndProp.lean
@@ -1757,6 +1757,12 @@ theorem Perm3_symm : ∀ (α : Type) (l₁ l₂ : List α),
   case perm3_trans _ _ _ _ _ ih₁2 ih₂3 =>
     exact Perm3.perm3_trans _ _ _ ih₂3 ih₁2
 
+
+-- DEV
+-- RAB & DHS: swap to Lean's `In` predicate (∈) rather than our own
+-- Also, we might use this as a running example in Automation.
+-- /DEV
+
 /- EX2 (Perm3_In) -/
 /- If you find yourself dealing with deeply nested `cases` in this proof,
    think back to `Logic` where you learned about the `obtain` tactic -/
