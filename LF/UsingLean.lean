669c8a70aad570827f4e8231470bd67a621eaf7e
Potato Hatsue <berberman@yandex.com>
2026-07-23 20:31:11 -0400

    Rework `sf_expect_failure` and `sf_experiment` syntax (#128)
    * Rework `sf_expect_failure` and `sf_experiment` syntax
    
    * Replace some guard_msgs with verso code blocks in UsingLean
    
    * Don't over consume ws
    
    ---------
    
    Co-authored-by: Benjamin Pierce <bcpierce00@users.noreply.github.com>

--- LF/UsingLean.lean
+++ LF/UsingLean.lean
@@ -600,18 +600,7 @@ Let's look at an example using `induction`.
 ::::full
 For example, suppose we start with the following incomplete proof:
 
-```lean
-/--
-error: unsolved goals
-case zero
-⊢ eqb 0 0 = true
-
-case succ
-n✝ : Nat
-a✝ : eqb n✝ n✝ = true
-⊢ eqb (n✝ + 1) (n✝ + 1) = true
--/
-#guard_msgs(error) in
+```lean -keep +error
 theorem foo (n : Nat) : eqb n n := by
   induction n
 ```
@@ -623,8 +612,6 @@ If you choose this action,
 Lean adds an explicit branch for each constructor:
 
 ```lean
-/-- warning: declaration uses `sorry` -/
-#guard_msgs in
 example (n : Nat) : eqb n n := by
   induction n with
   | zero => sorry
@@ -653,32 +640,14 @@ inaccessible names available again.
 The same trick also works for `match` expressions.
 For example, suppose we start with
 
-```
+```lean -keep +error
 def isZero (n : Nat) : Bool :=
   match n
 ```
 
-:::dev
-HIDE:
-@berberman: Incomplete `match` term would cause parse errors which `#guard_msgs` can't suppress.
-Use code block in docstring for now.
-:::
-
 Lean can generate the missing branches:
 
-```lean
-/--
-error: don't know how to synthesize placeholder
-context:
-n✝ n : Nat
-⊢ Bool
----
-error: don't know how to synthesize placeholder
-context:
-n : Nat
-⊢ Bool
--/
-#guard_msgs in
+```lean -keep +error
 def isZero (n : Nat) : Bool :=
   match n with
   | 0 => _
