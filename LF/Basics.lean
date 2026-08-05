bceea472d1199741615ade6affe79800eddb6958
Potato Hatsue <1793913507@qq.com>
2026-08-05 15:34:01 -0700

    More on structure

--- LF/Basics.lean
+++ LF/Basics.lean
@@ -1300,7 +1300,8 @@ end Playground
 :::
 
 :::full
-When defining an inductive type with just case, we can instead use a `structure`:
+When defining an inductive type with just one constructor, we can instead use a `structure`:
+
 ```lean
 structure NibbleStruct : Type where
   x0 : Playground.Bit
@@ -1308,7 +1309,8 @@ structure NibbleStruct : Type where
   x2 : Playground.Bit
   x3 : Playground.Bit
 ```
-Rather than construct this as `.bits .b0 .b0 .b0 .b0` we construct it as:
+
+Rather than construct this as `.bits .b0 .b0 .b0 .b0`, we construct it as:
 
 ```lean (name := nbs)
 #check NibbleStruct.mk .b0 .b0 .b0 .b0
@@ -1319,6 +1321,48 @@ Rather than construct this as `.bits .b0 .b0 .b0 .b0` we construct it as:
 ```
 
 The `.mk` constructor is created for us.
+However, structures are more commonly constructed by assigning values to their _fields_.
+Each field name is paird with its value using `:=`:
+
+
+```lean
+def zeroNibble : NibbleStruct := {
+    x0 := .b0
+    x1 := .b0
+    x2 := .b0
+    x3 := .b0
+  }
+```
+
+Since the result type is declared to be {name}`NibbleStruct`, Lean knows
+which structure and fields we mean. Unlike {name}`NibbleStruct.mk`,
+this construction syntax doesn't depend on the order of fields.
+
+Now that we have seen how to construct a strucure from scratch —
+how do we "update" an existing structure, or in other words, construct a new structure
+while reusing some old fields?
+
+```lean
+def setFistTwoBits (old : NibbleStruct)
+    (newX0 : Playground.Bit)
+    (newX1 : Playground.Bit) : NibbleStruct :=
+  { old with x0 := newX0, x1 := newX1 }
+```
+
+The expression `{ old with ... }` constructs a new {name}`NibbleStruct` whose `x0` and `x1`
+have the given value and whose other fields are copied from `old`.
+Keep in mind that `old` was not modified — we constructed a new one.
+
+```lean
+def makeNibbleStruct (x0 x1 x2 x3 : Playground.Bit) : NibbleStruct :=
+  { x0, x1, x2, x3 }
+```
+
+When a field an the variable supplying its value have the same name,
+Lean let us write just the name.
+Thus `{ x0, x1, x2, x3 }` is a shorthand for `{ x0 := x0, x1 := x1, x2 := x2, x3 := x3 }`.
+This is called _field abbreviation_.
+
 :::
 
 ## Natural Numbers
