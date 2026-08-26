dbb3cc4b3d581c5c8aac4481c61cf29d30a0d443
Benjamin Pierce <bcpierce00@users.noreply.github.com>
2026-08-26 00:26:43 +0000

    Explain :::details before first use.

--- LF/Basics.lean
+++ LF/Basics.lean
@@ -312,8 +312,13 @@ inductive MyBool : Type where
   | true
   | false
 ```
-
+:::full
+We next invoke a couple of Lean directives to help control formatting.  Exactly what these directives mean is not important for present purposes -- you can understand everything in the rest of the book without knowing -- so we will mark these commands -- and similar bits later on -- with `THESE DETAILS CAN BE SKIPPED` comments in `.lean` files, and with folded-up segments in the HTML presentation. Feel free to have a peek if you want (click on the triangle in the HTML to unfold it), or just jump down to the following material and keep going.
+:::
 :::details
+:::dev "Benjamin Pierce (bcpierce00)"
+Why is the explanation inside the lean block?  And can it include a sentence about the set_option?
+:::
 ```lean -show
 -- This is included to be able to format expressions involving these variables later
 variable (b : MyBool) (n m : Nat)
@@ -395,7 +400,7 @@ example : (!MyBool.false) = MyBool.true := by rfl
 ```
 
 ::::full
-The technical details of how these symbolic notations work are not something you need to understand until quite a bit later in your Lean journey.  We'll mark these details -- and similar material later on -- with `THESE DETAILS CAN BE SKIPPED` comments in `.lean` files and with collapsed text segments in the HTML presentation. Click on the triangle in the HTML if you want to have a peek, or just move on to the following material.
+The technical details of how these symbolic notations work are not something you need to understand until quite a bit later in your Lean journey.  
 
 :::details
 Lean has a very flexible notation system. Operators like `||` and `&&`
