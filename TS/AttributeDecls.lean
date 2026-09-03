faca195ceb69f06a5454b6b00322c091f54f0743
Daniel Sainati <sainatidaniel@gmail.com>
2026-09-03 12:35:43 -0400

    Initial Port of MoreStlc (#349)
    * start morestlc
    
    * morestlc
    
    * Update TS/MoreStlc.lean
    
    Co-authored-by: Jonathan Chan <jonathanhwchan@gmail.com>
    
    * fix build
    
    * fix build again
    
    * add grading holes
    
    * apply_rules
    
    * normalize in smallstep
    
    * normalize and apply_rules in stlc
    
    * add file lol
    
    * normalize/apply_rules
    
    * weakening
    
    * finish proofs
    
    * bit of automation
    
    * golf
    
    * golf
    
    * improve normalize tactic
    
    * subgoals
    
    * more subgoal formating
    
    * fix delaboration
    
    * break up big full block
    
    ---------
    
    Co-authored-by: Jonathan Chan <jonathanhwchan@gmail.com>

--- TS/AttributeDecls.lean
+++ TS/AttributeDecls.lean
@@ -8,3 +8,7 @@ register_label_attr SimpleArith
 -- Stlc.lean
 register_label_attr StlcEval
 register_label_attr StlcTyping
+
+-- MoreStlc.lean
+register_label_attr ExtStlcEval
+register_label_attr ExtStlcTyping
