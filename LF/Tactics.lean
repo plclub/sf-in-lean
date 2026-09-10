2ca8d7b7dd28a30be6b798dd287ccf7cd0fab95d
Michael Hicks <mwh@cis.upenn.edu>
2026-09-10 09:10:53 -0400

    Tactics: Tidying
    - Swap `if` with `bif`
    - Use `cases ... with` rather than with `.`
    - Clarified a couple of examples
    - Missing header

--- LF/Tactics.lean
+++ LF/Tactics.lean
@@ -171,6 +171,16 @@ example (n m o : Nat)
     rw [h₁, h₃]
 ```
 
+Note that both {tactic}`injection` and {tactic}`injections` will simplify
+a hypothesis before applying injectivity. Thus we could also use them
+to solve the following example, which requires simplifying the `++` and {lean}`List.reverse` expressions:
+
+```lean
+example (n m o : Nat)
+    (h : [n] ++ [m] = List.reverse ([o] ++ [o])) :
+    n = m := by sorry
+```
+
 :::dev "Benjamin Pierce (bcpierce00)" PotentialImprovement
 How do I predict how many equations will be generated, and in what order?  (E.g., how did I know to write `_` in the above example?) I suspect the real answer is "try it without the `with`, see what happens, then go back and tidy." If so, it would be less puzzling just to say so.
 :::
@@ -194,6 +204,8 @@ theorem injection_ex3 {α : Type} (x y z : α) (l j : List α)
 :::
 :::::
 ::::::
+
+## Disjointness
 
 ::::full
 So much for injectivity of constructors.  What about disjointness?
@@ -540,54 +552,37 @@ We can specify the recursion-depth with `congr n`.
 ::::
 
 ```lean +error (name := congr1)
-example (a b c d : Nat) (hab : a = b) (hcd : c = d) :
-    (a, c + 1) = (b, 1 + d) := by
+example (a b c d : Nat) (h1 : a = b + 1) (h2 : d = c + 1) :
+    (a + c, true) = (b + d, true) := by
   congr
 ```
 
-We now have three goals: `c = 1`, `1 = d`, and `1 = d`,
+We now have two goals: `a = b` and `c = d`,
 but these are not provable from our hypotheses! {tactic}`congr`
 has gone too deep.
 
 ```leanOutput congr1
 unsolved goals
-case e_snd.e_a
+case e_fst.e_a
 a b c d : Nat
-hab : a = b
-hcd : c = d
-⊢ c = 1
+h1 : a = b + 1
+h2 : d = c + 1
+⊢ a = b
 
-case e_snd.e_a.e_2
+case e_fst.e_a
 a b c d : Nat
-hab : a = b
-hcd : c = d
-⊢ 1 = d
-
-case e_snd.e_a.e_3
-a b c d : Nat
-hab : a = b
-hcd : c = d
-⊢ 1 = d
+h1 : a = b + 1
+h2 : d = c + 1
+⊢ c = d
 ```
 
 ```lean
-example (a b c d : Nat) (hab : a = b) (hcd : c = d) :
-    (a, c + 1) = (b, 1 + d) := by
+example (a b c d : Nat) (h1 : a = b + 1) (h2 : d = c + 1) :
+    (a + c, true) = (b + d, true) := by
   /- Using `congr` shallowly allows us to complete the proof -/
   congr 1
-  rw [Nat.add_comm]
-  congr
+  rw [h1, h2, Nat.add_assoc, Nat.add_comm 1 c]
 ```
-
-:::dev "Niklas Halonen (xhalo32)"
-The above proof can be made simpler by just rewriting before the `congr`, so arguably it doesn't require limiting the depth.
-```lean
-example (a b c d : Nat) (hab : a = b) (hcd : c = d) :
-    (a, c + 1) = (b, 1 + d) := by
-  rw [Nat.add_comm]
-  congr
-```
-:::
 
 # Using {tactic}`cases` on Expressions
 
@@ -606,7 +601,7 @@ variables:
 
 ```lean
 def chooseIf {α : Type} (test : α → Bool) (x y : α) : α :=
-  if test x then x else y
+  bif test x then x else y
 
 theorem chooseIf_self {α : Type} (test : α → Bool) (x : α) :
     chooseIf test x x = x := by
@@ -616,7 +611,7 @@ theorem chooseIf_self {α : Type} (test : α → Bool) (x : α) :
 
 ::::full
 After unfolding {name}`chooseIf` in the above proof, we find that
-we are stuck on `(if test x = true then x else x) = x`.  But either
+we are stuck on `(bif test x then x else x) = x`.  But either
 `test x` is `true` or it isn't,
 so we can use `cases (test x)` to let us reason about the two cases.
 
@@ -693,13 +688,8 @@ theorem zip_unzip' {α β : Type} (l : List (α × β))
 
 When using {tactic}`cases`, we can specify to Lean that it should
 remember an equality between a compound expression and what we are
-decomposing it into, using `cases h : ...` syntax. This step can actually be critical: if we leave it out, we might lack
+decomposing it into, using `cases h : ...` syntax. This step is sometimes critical: if we leave it out, we might lack
 information we need to complete a proof.
-
-:::dev "Benjamin Pierce (bcpierce00)"
-Students might then wonder why we are teaching them the non-`with` syntax at all...
-:::
-
 
 ::::full
 For example, suppose we define a function `keepIf` like this:
@@ -707,7 +697,7 @@ For example, suppose we define a function `keepIf` like this:
 
 ```lean
 def keepIf {α : Type} (test : α → Bool) (x : α) : Option α :=
-  if test x then some x else none
+  bif test x then some x else none
 ```
 
 ::::full
@@ -728,14 +718,14 @@ case false
 α : Type
 test : α → Bool
 x y : α
-h : (if test x = true then some x else none) = some y
+h : (bif test x then some x else none) = some y
 ⊢ x = y
 
 case true
 α : Type
 test : α → Bool
 x y : α
-h : (if test x = true then some x else none) = some y
+h : (bif test x then some x else none) = some y
 ⊢ x = y
 ```
 
@@ -761,13 +751,15 @@ theorem keepIf_some {α : Type} (test : α → Bool) (x y : α)
     (h : keepIf test x = some y) :
     x = y := by
   rw [keepIf] at h
-  cases hTest : test x
   -- Now we have the same state as at the point where we got stuck
   -- above, except that the context contains an extra equality
   -- assumption, which is exactly what we need to make progress.
-  · rw [hTest] at h
+  cases hTest : test x with
+  | false =>
+    rw [hTest] at h
     contradiction
-  · rw [hTest] at h
+  | true =>
+    rw [hTest] at h
     injections
 ```
 
@@ -1401,7 +1393,7 @@ ih : n'.double = (m' + 1).double → n' = m' + 1
 
 From `h`, using the definition of {name}`Nat.double` we can obtain
 :::dev "Benjamin Pierce (bcpierce00)"
-Formatting: The displayed material in this section is inconsistent and rather ugly -- some of it is indented, some not, some is bulleted, some not.  I think this is mostly a hold-over from the Rocq formatting markup, which was rather problematic in similar ways. 
+Formatting: The displayed material in this section is inconsistent and rather ugly -- some of it is indented, some not, some is bulleted, some not.  I think this is mostly a hold-over from the Rocq formatting markup, which was rather problematic in similar ways.
 :::
 
 
